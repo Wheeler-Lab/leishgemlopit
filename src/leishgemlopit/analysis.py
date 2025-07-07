eaca39b285ae93547f0273cee36ab16ba3e30e5b
Ulrich Dobramysl <1979498+ulido@users.noreply.github.com>
2025-07-07 08:53:38 +0000

    Output to ZIP file for CLI

--- src/leishgemlopit/analysis.py
+++ src/leishgemlopit/analysis.py
@@ -1,6 +1,8 @@
 from collections import Counter, defaultdict
 import argparse
 import pathlib
+import re
+from zipfile import ZipFile
 
 from .constants import FIXTURE_PATH
 from .supervised import SupervisedTAGMCollection
@@ -54,6 +56,8 @@ class LmexMultiSheetExcelMarkerTermFile(MarkerGenerator):
 
 
 def analysis(run_name: str, lopit_data: pd.DataFrame):
+    run = LOPITRun.from_dataframe(run_name, lopit_data)
+
     markerfactory = MarkerFactory([
         TrypTagMarkerFactory(),
         TBruceiMarkerTermFile(
@@ -65,27 +69,99 @@ def analysis(run_name: str, lopit_data: pd.DataFrame):
         )
     ])
 
-    run = LOPITRun.from_dataframe(run_name, lopit_data)
-
-    tsne = TSNEAnalysis(run)
-    tsne.to_png(f"{run.name}_tsne.png")
-
-    markers = Markers(markerfactory, list(run.genes), tsne)
-    markercounter = Counter[str](
-        [m for mset in markers.values() for m in mset])
-    for m, c in markercounter.most_common():
-        print(m, c)
-    markers.to_png(f"{run.name}_markers.png")
-
-    clusters = UnsupervisedHDBSCAN(run, tsne, min_samples=3, markers=markers)
-    clusters.to_png(f"{run.name}_unsupervised_clusters.png")
-
-    assigned = SupervisedTAGMCollection(
-        run,
-        markers,
-        tsne_analysis=tsne,
+    descriptions = pd.Series(
+        {
+            gene.gene_id: gene.description if gene.description != "unknown" else ""
+            for gene in {
+                gene for experiment in run.values()
+                for gene in experiment
+            }
+        },
+        name="description",
     )
-    assigned.to_png(f"{run.name}_supervised_clusters.png")
+
+    with ZipFile(run_name + ".zip", "w") as zip:
+        tsne = TSNEAnalysis(run)
+        tsne.to_png(zip.open(f"{run.name}_tsne.png", "w"))
+
+        markers = Markers(markerfactory, list(run.genes), tsne)
+        markers.to_png(zip.open(f"{run.name}_markers.png", "w"))
+
+        clusters = UnsupervisedHDBSCAN(run, markers, tsne, min_samples=3)
+        clusters.to_png(zip.open(f"{run.name}_unsupervised_clusters.png", "w"))
+
+        assigned = SupervisedTAGMCollection(
+            run,
+            markers,
+            tsne_analysis=tsne,
+        )
+        assigned.to_png(zip.open(f"{run.name}_supervised_clusters.png", "w"))
+
+        tables = {
+            "Gene": descriptions,
+        }
+        tables.update(
+            {
+                key: component.to_dataframe(include_tsne=False)
+                for key, component in [
+                    ("t-SNE", tsne),
+                    ("Markers", markers),
+                    ("Unsupervised clustering", clusters),
+                    ("TAGM-MAP", assigned),
+                ]
+            }
+        )
+        table = pd.concat(
+            tables,
+            axis=1,
+        )
+        table = table.sort_index().fillna("")
+        print(table.loc[:, ("Unsupervised clustering", "cluster")])
+        print(table.loc[:, ("Gene", "description")])
+        print(table.loc[:, ("Markers", "marker")])
+
+        nr_of_unknown_unsupervised = (
+            (
+                (table.loc[:, ("Unsupervised clustering", "cluster")] >= 0) &
+                (table.loc[:, ("Gene", "description")] == "") &
+                (table.loc[:, ("Markers", "marker")] == "")
+            ).sum()
+        )
+        nr_of_unknown_tagm = (
+            (
+                (table.loc[:, ("TAGM-MAP", "label")] != "") &
+                (table.loc[:, ("Gene", "description")] == "") &
+                (table.loc[:, ("Markers", "marker")] == "")
+            ).sum()
+        )
+        summary = pd.DataFrame(
+            [
+                {
+                    "Analysis": "Unsupervised clustering",
+                    "Number of unknown genes assigned": nr_of_unknown_unsupervised,
+                },
+                {
+                    "Analysis": "TAGM-MAP",
+                    "Number of unknown genes assigned": nr_of_unknown_tagm,
+                },
+            ]
+        ).set_index("Analysis")
+
+        markercounter = Counter[str](
+            [m for mset in markers.values() for m in mset])
+        markersummary = pd.Series(
+            {
+                marker: count
+                for marker, count in markercounter.most_common()
+            },
+            name="Count",
+        ).to_frame()
+
+        with zip.open(f"{run.name}_results.xlsx", "w") as excel_zip:
+            with pd.ExcelWriter(excel_zip) as excel:
+                markersummary.to_excel(excel, sheet_name="Markers summary")
+                summary.to_excel(excel, sheet_name="Analysis summary")
+                table.to_excel(excel, sheet_name="Assignments")
 
 
 def cli():
@@ -95,12 +171,21 @@ def cli():
 
     args = parser.parse_args()
 
-    df = pd.read_csv(args.lopit_data).set_index("geneid").iloc[:, :44]
-    df.columns = pd.MultiIndex.from_product(
-        [(1, 2, 3, 4), DEFAULT_TMT_LABELS],
-        names=["experiment", "tmt_labels"]
-    )
-    df = df[df.notna().all(axis=1)]
-    df: pd.DataFrame = df.stack("experiment", future_stack=True)
+    df = pd.read_csv(args.lopit_data) #.set_index("geneid").iloc[:, :44]
+    tmt_re = "|".join(DEFAULT_TMT_LABELS)
+    columns_re = re.compile(rf"Abundances \(Grouped\): (?P<experiment>[A-Z]), (?P<tmt>{tmt_re})$")
+    relevant_columns = {"UniProt Entry Name": "geneid"}
+    for column in df.columns:
+        if m := columns_re.match(column):
+            relevant_columns[column] = (m["experiment"], m["tmt"])
+    df = df.loc[:, list(relevant_columns)]
+    df.columns = list(relevant_columns.values())
+    df.loc[:, "geneid"] = df.geneid.str.replace("-t42_1-p1", "")
+    df = df.set_index("geneid").fillna(0.0)
+    df.columns = pd.MultiIndex.from_tuples(df.columns.tolist(), names=("experiment", "tmt_label"))
+    df = df.stack("experiment", future_stack=True)
+    df: pd.DataFrame = (df.T / df.sum(axis=1)).T
+    df = df.unstack("experiment")
+    df = df[df.notna().all(axis=1)].stack("experiment", future_stack=True)
 
     analysis(args.name, df)
